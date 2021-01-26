2f46d5cbd2590a58d9664211638479f7d15516b3
Francisco Melo <tomfram03@gmail.com>
2021-01-26 20:21:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    musicas: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('')
+    this.setState({ musicas: response.data.mus.week.all })
+  
+  }
+
+  render() {
+    
+    const { musicas } = this.state;
+
+    return (
+      <div>
+        <h1>Listar</h1>
+        {musicas.map(musica => (
+          <li key={musica.id}>
+            <h3>              
+              <p>Nome: {musica.name}</p>
+              <p>Artista: {musica.art.name}</p>
+              <p> <a href={musica.art.url} target="_blank">Ver artista</a></p>
+              <img src={musica.art.pic_medium}/>
+              <p> <a href={musica.art.url} target="_blank">Ver artista</a></p>
+            </h3>
+          </li>
+        ))}
+        
+      </div>
+    )
+  }
 }
 
 export default App;
+
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// Pode ser algum servidor executando localmente: 
+// http://localhost:3000
+
+const api = axios.create({
+  baseURL: 'https://api.vagalume.com.br/rank.php?apikey=660a4395f992ff67786584e238f501aa&type=mus&period=week&periodVal=201134&scope=all&limit=10',
+  /*headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+  }*/
+});
+
+export default api;
